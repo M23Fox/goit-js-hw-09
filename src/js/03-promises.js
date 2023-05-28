9bd2fdb49bee57ac1e69dc13c1b3b5abe81c8f51
Maryna Biesiedina <43865696+M23Fox@users.noreply.github.com>
2023-05-28 18:20:09 +0100

    9.2

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,25 @@
+// import Notiflix from 'notiflix';
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmitForm);
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  setTimeout(() => {
+    if (shouldResolve) {
+      // Fulfill
+    } else {
+      // Reject
+    }
+  }, delay);
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
